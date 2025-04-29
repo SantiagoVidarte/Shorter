68eee97b67ad4eeb908b4697bd2d0b406ad3efa6
smvidarte <s.vid10.54@gmail.com>
2025-04-29 17:50:58 -0300

    fixed auth

--- app/api/[shortId]/route.ts
+++ app/api/[shortId]/route.ts
@@ -3,24 +3,27 @@ export const runtime = 'nodejs';
 
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
+
 export async function GET(
-    request: NextRequest,
-    context: { params: Promise<{ shortId: string }> }
-  ) {
-    const { shortId } = await context.params;
-  
-    const entry = await prisma.shortUrl.findUnique({
-      where: { short: shortId },
-    });
-  
-    if (!entry) {
-      return NextResponse.redirect(new URL('/404', request.url));
-    }
-  
-    await prisma.shortUrl.update({
-      where: { short: shortId },
-      data: { clicks: { increment: 1 } },
-    });
-  
-    return NextResponse.redirect(entry.original);}
+  request: NextRequest,
+  { params }: { params: { shortId: string } }
+) {
+  const { shortId } = params;
+
+  const entry = await prisma.shortUrl.findUnique({
+    where: { short: shortId },
+  });
+
+  if (!entry) {
+    return NextResponse.redirect(new URL('/404', request.url));
+  }
+
+  await prisma.shortUrl.update({
+    where: { short: shortId },
+    data: { clicks: { increment: 1 } },
+  });
+
+  return NextResponse.redirect(entry.original);
+}
--- app/next-auth.d.ts
+++ app/next-auth.d.ts
@@ -1,15 +1,9 @@
-// next-auth.d.ts
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: DefaultSession["user"] & {
       id: string;
-      // aquí podrías añadir más campos si lo necesitas
     };
-  }
-
-  interface User extends DefaultUser {
-    id: string;
   }
 }
