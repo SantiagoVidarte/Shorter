c86838db494b39dda65fcf0735aa1311da80c23f
smvidarte <s.vid10.54@gmail.com>
2025-04-29 12:51:17 -0300

    fixed generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-'use client';
-import { useState, useRef } from 'react';
+"use client";
+import { useState, useRef } from "react";
 import Image from "next/image";
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [shortUrl, setShortUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState("");
 
   interface ShortUrlResponse {
     shortUrl: string;
@@ -13,9 +13,9 @@ export default function Home() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const url = inputRef.current ? inputRef.current.value : null;
-    fetch('/api/shortUrl', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("/api/shortUrl", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ url }),
     })
       .then((res) => res.json() as Promise<ShortUrlResponse>)
@@ -25,16 +25,16 @@ export default function Home() {
   };
   const copyToClipboard = () => {
     if (shortUrl) {
-      navigator.clipboard.writeText(shortUrl)
+      navigator.clipboard
+        .writeText(shortUrl)
         .then(() => {
-          alert('Short URL copied to clipboard!');
+          alert("Short URL copied to clipboard!");
         })
         .catch((err) => {
-          console.error('Failed to copy: ', err);
+          console.error("Failed to copy: ", err);
         });
     }
   };
-
 
   return (
     <>
@@ -44,31 +44,40 @@ export default function Home() {
           <p className="col-span-2  ">Short your urls here.</p>
 
           <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <form onSubmit={handleSubmit} className="gap-4 flex items-center flex-col sm:flex-row">
-              <input type="text" ref={inputRef} placeholder="Enter your URL here" className="border-2 border-gray-300 rounded-md p-2 w-full sm:w-[400px]" />
-              <button type="submit" className="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600 transition duration-200">Shorten</button>
-
+            <form
+              onSubmit={handleSubmit}
+              className="gap-4 flex items-center flex-col sm:flex-row"
+            >
+              <input
+                type="text"
+                ref={inputRef}
+                placeholder="Enter your URL here"
+                className="border-2 border-gray-300 rounded-md p-2 w-full sm:w-[400px]"
+              />
+              <button
+                type="submit"
+                className="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600 transition duration-200"
+              >
+                Shorten
+              </button>
             </form>
-
           </div>
-            {shortUrl && (
-              <div className=" col-span-2 p-4 m-2 gap-2">
-                <span>{shortUrl}</span>
-                <Image
+          {shortUrl && (
+            <button
+              onClick={copyToClipboard}
+              className=" col-span-2 p-4 m-2 gap-2"
+            >
+              <Image
                 svg="copy"
-                  alt="Copy to clipboard"
-                  type="button"
-                  onClick={copyToClipboard}
-                  className="bg-blue-500 cursor-pointer"
-                >
-                </Image>
-              </div>
-            )}
+                alt="Copy to clipboard"
+                type="button"
+                className="bg-blue-500 cursor-pointer"
+              ></Image>
+            </button>
+          )}
         </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-
-        </footer>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
       </div>
     </>
   );
-};
+}
